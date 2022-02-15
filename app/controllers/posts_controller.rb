63c20dc538bf2a730c34cfd6ea1a55faff178a82
kbwedge <kevin.wedge@kellogg.northwestern.edu>
2022-02-15 17:23:57 +0000

    New Contact

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,15 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @posts = Post.new
+  end
+
+  def create
+    @posts = Post.new(params["post"])
+    @posts.save
+    redirect_to "/posts"
+  end
+
+
 end
